61ab7038dd767d41424c7cc1c74657b20ba23820
Navadeep Bommarapu <153001918+Navadeep-bommarapu@users.noreply.github.com>
2026-05-08 22:49:17 +0530

    Basic Hashing Problems

---
+++ hashing.py
@@ -0,0 +1,79 @@
+# Hashing
+# Finding the occurence of a number in the list 
+# arr = [1,2,1,3,2]
+# 1 -> 2 times
+# 2 -> 2 times
+# 3 -> 1 time
+# 4 -> 0 times
+
+# Brute Force Approach
+# arr = list(map(int, input("Enter list values: ").split()))
+# number = list(map(int, input("Enter queries: ").split()))
+
+
+def num_occurence(number, arr):
+    count = 0
+    for i in arr:
+        if i == number:
+            count += 1
+    return count
+
+# print(num_occurence(number=1, arr=[1,2,1,3,1,2]))
+
+def hashing_number(number, arr):
+    hash = {}
+    
+    for i in arr:
+        if i in hash:
+            hash[i] += 1
+        else:
+            hash[i] = 1
+    
+    for i in number:
+        if i > max(arr):
+            print(0)
+        else:
+            print(hash[i])
+        
+    
+# hashing_number(number, arr)
+# string = input("Enter the string: ")
+# q = list(input("Enter queries: ").split())
+# print(q)
+
+
+# hashing_with_string(string, q)
+def hashing_with_string(str, queries):
+    hash = {}
+    
+    for i in str:
+        hash[i] = hash.get(i,0) + 1
+    
+    for i in hash:
+        print(f"{i} -> {hash[i]}")
+    
+    for query in queries:
+        if hash.get(query):
+            print(hash[query])
+        else:
+            print(0)
+            
+            
+# hashing_with_string("string", "i")         
+
+nums = [2,3,4]
+target = 7
+
+def twoSum(nums, target):
+    hash_map = {}
+    
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in hash_map:
+            return [hash_map[complement], i]
+        
+        hash_map[nums[i]] = i
+
+# print(twoSum(nums, target))
+
