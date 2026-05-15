6d1b90c940a021e09973c94b252764185c0d694d
Navadeep Bommarapu <153001918+Navadeep-bommarapu@users.noreply.github.com>
2026-05-15 23:00:29 +0530

    Medium Array Problems

--- medium_arrays.py
+++ medium_arrays.py
@@ -37,7 +37,6 @@ def optimal_twoSum(arr, target):
 
     print(arr)
     while left < right:
-        
         if arr[left] + arr[right] == target:
             return True
         elif (arr[left] + arr[right] ) < target:
@@ -50,9 +49,9 @@ def optimal_twoSum(arr, target):
 
 # Sort an Array of 0's, 1's and 2's
 # Brute Approach - using any of the sorting algorithms
+arr = [1,2,1,0,0,1,2,0,1]
 low = 0
 high = len(arr) - 1
-arr = [1,2,1,0,0,1,2,0,1]
 
 def merge(arr, low, mid, high):
     temp = []
@@ -82,7 +81,7 @@ def merge(arr, low, mid, high):
 def sort_arr(arr, low, high):
     # merge sort
     
-    if low >= high:
+    if low == high:
         return
 
     mid = (low + high) // 2
@@ -94,4 +93,117 @@ def sort_arr(arr, low, high):
     return arr
 
 
-print(sort_arr(arr, low, high))
+# print(sort_arr(arr, low, high))
+
+# Better Approach - counting the 0's, 1's and 2's in the arr and store in place of the count0, count1, count2
+arr = [1,2,1,0,0,1,2,0,1]
+def better_sort_arr(arr):
+    count0 = 0
+    count1 = 0
+    count2 = 0
+    
+    for i in arr:
+        if i == 0:
+            count0 += 1
+        elif i == 1:
+            count1 += 1
+        else:
+            count2 += 1
+        
+    for i in range(count0):
+        arr[i] = 0
+    
+    for i in range(count1):
+        arr[i+count0] = 1
+    
+    for i in range(count2):
+        arr[i + count0 + count1] = 2
+
+    return arr
+
+# print(better_sort_arr(arr))
+
+# Optimal Approach - using Dutch National Flag Algorithm
+# Dutch National Flag Algorithm Rules:
+# 1. range(0 - low - 1) - everything is 0 (sorted)
+# 2. range(low, mid - 1) - everything is 1 (sorted)
+# range(mid, high) - unsorted array
+# 3. range(high + 1, len(arr) - 1) - everything is 2 (sorted)
+arr = [1,2,1,0,0,1,2,0,1]
+def optimal_sort_arr(arr):
+    low = 0
+    mid = 0
+    high = len(arr) - 1
+    
+    for i in range(len(arr)):
+        if arr[mid] == 0:
+            arr[mid], arr[low] = arr[low], arr[mid]
+            mid += 1
+            low += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+
+    return arr
+
+# print(optimal_sort_arr(arr))
+
+
+# Majority Element in Array
+# Brute Approach - iterating through the array and check the element count is greater than the lenght of array by 2
+arr = [6,5,5]
+def majority_element(arr):
+    n = len(arr)
+    for i in arr:
+        count = 0
+        for j in arr:
+            if j == i:
+                count += 1
+            
+        if count > n//2:
+            return arr[i]
+    
+    return -1
+
+print(majority_element)
+
+# Better Approach - using Hash Map storing the arr element and number of times it present in the arr, 
+#                   and again iterating through the map and seeing the element count > n//2 or can check the max, 
+#                   maxelement variable with the value, element in the hashmap
+def better_majority_element(arr):
+    hash_map = {}
+    
+    for i in range(len(arr)):
+        if arr[i] in hash_map:
+            # print(arr[i])
+            hash_map[arr[i]] += 1
+        else:
+            hash_map[arr[i]] = 1
+    
+    print(hash_map)
+    max = 0
+    maxelement = 0
+    for element, value in hash_map.items():
+        if value > max:
+            max = value
+            maxelement = element
+            
+    return maxelement
+        
+print(better_majority_element(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
